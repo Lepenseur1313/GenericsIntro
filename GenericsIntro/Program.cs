32781d90ed0219a0714b22c681d5d259714f1f2d
Mesud Tekbaş <lepenseurgame@outlook.com>
2021-01-29 02:13:30 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Burak");
+
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
